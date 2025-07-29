08924edf1b7cc415614532a687e8049f72b0db1b
Paul van Mierlo <paulvanmierlo@MacBookPro.home>
2025-07-29 19:35:01 +0200

    corrected logos

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cryptologos.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   webpack: (config) => {
